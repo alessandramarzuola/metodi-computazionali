67b10f88ac9c812d381aaf853425d195eb060531
alessandramarzuola <am010021@infolab01>
2022-10-19 17:46:38 +0200

    prova grafico

---
+++ esercitazione2/lez19ott.py
@@ -0,0 +1,9 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+arrayx=np.array([1,2,3,4,5])
+arrayy=np.array([6,7,8,9,10])
+plt.plot(arrayx,arrayy)
+plt.xlabel('x')
+plt.ylabel('y')
+plt.show()
